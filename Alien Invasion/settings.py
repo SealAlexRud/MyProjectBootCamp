e65ca2538ae28010aa0e84944c5dee2cd04c06c2
SealAlexRud <138847798+SealAlexRud@users.noreply.github.com>
2023-07-07 12:50:27 +0700

    Add files via upload

---
+++ Alien Invasion/settings.py
@@ -0,0 +1,28 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        # Настройки корабля
+        self.ship_speed = 10
+        self.ship_limit = 3
+        # Параметры снаряда
+        self.bullet_speed = 10
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 0, 0)
+        self.bullets_allowed = 3
+        # Настройки пришельцев
+        self.alien_speed = 10
+        self.fleet_drop_speed = 30
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        #подсчет очков
+        self.alien_points = 1
